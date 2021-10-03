a363bf37112ca0be8fd0c4ac3056fef0706489a0
GhostLove <19521238@gm.uit.edu.vn>
2021-10-03 16:42:35 +0700

    fix config

--- config/db.config.js
+++ config/db.config.js
@@ -8,5 +8,29 @@ module.exports = {
     min: 0,
     acquire: 30000,
     idle: 10000
-  }
+  },
+  development: {
+    dialect: process.env.DB_DIALECT,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME_DEVELOPMENT,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+  },
+  test: {
+    dialect: process.env.DB_DIALECT,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME_DEVELOPMENT,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+  },
+  production: {
+    dialect: process.env.DB_DIALECT,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME_DEVELOPMENT,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+  },
 };
